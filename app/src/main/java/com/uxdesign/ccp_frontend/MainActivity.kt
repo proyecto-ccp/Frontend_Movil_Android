b393c6b4f14bf4379283269eded3a47ff4a01d08
NataliaRojas <np.rojas1158@uniandes.edu.co>
2025-03-30 15:38:06 -0500

    Interfaz base

---
+++ app/src/main/java/com/uxdesign/ccp_frontend/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.uxdesign.ccp_frontend
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.uxdesign.cpp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val buttonIngresar: Button = findViewById(R.id.buttonIngresar)
+        buttonIngresar.setOnClickListener {
+            val intent = Intent(this, MenuActivity::class.java)
+            startActivity(intent)
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
