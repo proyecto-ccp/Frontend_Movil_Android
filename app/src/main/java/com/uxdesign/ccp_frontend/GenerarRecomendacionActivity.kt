dfb3c4714b322c99cf40651f3edcb90ae5c4c8c8
NataliaRojas <np.rojas1158@uniandes.edu.co>
2025-04-03 10:36:22 -0500

    Se completa activity para mostrar recomendacion

--- app/src/main/java/com/uxdesign/ccp_frontend/GenerarRecomendacionActivity.kt
+++ app/src/main/java/com/uxdesign/ccp_frontend/GenerarRecomendacionActivity.kt
@@ -1,6 +1,7 @@
 package com.uxdesign.ccp_frontend
 
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -12,6 +13,13 @@ class GenerarRecomendacionActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_generar_recomendacion)
+
+        //val imagen: ImageView = findViewById(R.id.imagenRecomendacion)
+
+        //val bitmap = intent.getParcelableExtra<Bitmap>("imageBitmap")
+        //imagen.setImageBitmap(bitmap)
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
