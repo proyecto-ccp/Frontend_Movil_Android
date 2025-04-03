0a248aa1943efc1f2e0629860970b3f9fd96a2c2
NataliaRojas <np.rojas1158@uniandes.edu.co>
2025-04-03 16:07:26 -0500

    Binding boton menu con catalogo

--- app/src/main/java/com/uxdesign/ccp_frontend/MenuActivity.kt
+++ app/src/main/java/com/uxdesign/ccp_frontend/MenuActivity.kt
@@ -21,6 +21,12 @@ class MenuActivity : AppCompatActivity() {
         //    startActivity(intent)
         //}
 
+        val buttonCatalogo: Button = findViewById(R.id.buttonPedido)
+        buttonCatalogo.setOnClickListener {
+            val intent = Intent(this, CatalogoProductosActivity::class.java)
+            startActivity(intent)
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
