5f76459987f910b040674b6af7133def49a4171e
NataliaRojas <np.rojas1158@uniandes.edu.co>
2025-04-04 16:45:33 -0500

    Item pedido para mostrar detalle del producto en el pedido

--- app/src/main/java/com/uxdesign/ccp_frontend/VerPedidoActivity.kt
+++ app/src/main/java/com/uxdesign/ccp_frontend/VerPedidoActivity.kt
@@ -1,10 +1,14 @@
 package com.uxdesign.ccp_frontend
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.uxdesign.cpp.R
 
 class VerPedidoActivity : AppCompatActivity() {
@@ -12,6 +16,16 @@ class VerPedidoActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_ver_pedido)
+
+        val buttonFin: Button = findViewById(R.id.buttonFin)
+        buttonFin.setOnClickListener {
+            val intent = Intent(this, CatalogoProductosActivity::class.java)
+            startActivity(intent)
+        }
+
+        val recyclerView: RecyclerView = findViewById(R.id.recyclerViewProductosPedido)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
