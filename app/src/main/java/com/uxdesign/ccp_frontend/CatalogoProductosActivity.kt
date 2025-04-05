bd351ba009ea31adac9ef6f901f3edb1e724c95f
NataliaRojas <np.rojas1158@uniandes.edu.co>
2025-04-05 13:35:01 -0500

    Se adelanta Accesibilidad en crear pedido y se deja lista para terminar cuando se manejen roles para diferenciar cliente de fuerza de ventas

--- app/src/main/java/com/uxdesign/ccp_frontend/CatalogoProductosActivity.kt
+++ app/src/main/java/com/uxdesign/ccp_frontend/CatalogoProductosActivity.kt
@@ -2,9 +2,12 @@ package com.uxdesign.ccp_frontend
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -17,8 +20,29 @@ class CatalogoProductosActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_catalogo_productos)
 
+        //Adaptabilidad
+
+        val mainLayout: ConstraintLayout = findViewById(R.id.main)
         val buttonFinalizar: Button = findViewById(R.id.botonFinalizar)
-        buttonFinalizar.setOnClickListener {
+        val buttonPedido: Button = findViewById(R.id.botonPedido)
+        val imageEye: ImageView = findViewById(R.id.imageOjoN)
+
+        imageEye.visibility = View.VISIBLE
+
+        imageEye.setOnClickListener {
+
+            // mainLayout.setBackgroundColor(resources.getColor(R.color.darkgrey, null))
+           // buttonFinalizar.setBackgroundColor(resources.getColor(R.color.greytext, null))
+           // buttonPedido.setBackgroundColor(resources.getColor(R.color.greytext, null))
+            imageEye.setImageResource(R.drawable.blackeye)
+            imageEye.visibility = View.GONE
+        }
+
+        //User Interface Compras
+        // mainLayout.setBackgroundColor(resources.getColor(R.color.orange, null))
+        //imageEye.visibility = View.VISIBLE
+
+         buttonFinalizar.setOnClickListener {
             val intent = Intent(this, FinalizarPedidoActivity::class.java)
             startActivity(intent)
         }
