7dc60db0b9e2d041a7e96db7af1bcd5861a70a11
NataliaRojas <np.rojas1158@uniandes.edu.co>
2025-04-22 08:51:24 -0500

    Se implementa detalle de clienteal hacer clic en item de la lista

--- app/src/main/java/com/uxdesign/ccp_frontend/ClienteAdapter.kt
+++ app/src/main/java/com/uxdesign/ccp_frontend/ClienteAdapter.kt
@@ -17,15 +17,19 @@ class ClienteAdapter (private val clientes: List<Cliente>) : RecyclerView.Adapte
     override fun onBindViewHolder(holder: ClienteViewHolder, position: Int) {
         val cliente = clientes[position]
         holder.bind(cliente)
-        val context = holder.itemView.context
 
         holder.itemView.setOnClickListener {
+            val context = holder.itemView.context
             val intent = Intent(context, DetalleClienteActivity::class.java).apply {
                 putExtra("cliente_id", cliente.id)
                 putExtra("cliente_nombre", cliente.nombre)
                 putExtra("cliente_tipodoc", cliente.tipoDocumento)
-                putExtra("cliete_documento", cliente.documento)
+                putExtra("cliente_documento", cliente.documento)
+                putExtra("cliente_telefono", cliente.telefono)
                 putExtra("cliente_direccion", cliente.direccion)
+                putExtra("cliente_ciudad", cliente.idCiudad)
+                putExtra("cliente_zona", cliente.idZona)
+                putExtra("cliente_correo", cliente.email)
             }
             context.startActivity(intent)
         }
@@ -36,8 +40,8 @@ class ClienteAdapter (private val clientes: List<Cliente>) : RecyclerView.Adapte
     }
 
     class ClienteViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        private val nombreCliente: TextView = itemView.findViewById(R.id.nombreProducto)
-        private val ciudadCliente: TextView = itemView.findViewById(R.id.descripcionProducto)
+        private val nombreCliente: TextView = itemView.findViewById(R.id.nombreCliente)
+        private val ciudadCliente: TextView = itemView.findViewById(R.id.ciudadCliente)
 
         fun bind(cliente: Cliente) {
             nombreCliente.text = cliente.nombre
--- app/src/main/java/com/uxdesign/ccp_frontend/DetalleClienteActivity.kt
+++ app/src/main/java/com/uxdesign/ccp_frontend/DetalleClienteActivity.kt
@@ -1,6 +1,7 @@
 package com.uxdesign.ccp_frontend
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -12,6 +13,25 @@ class DetalleClienteActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_detalle_cliente)
+
+        val nombre = intent.getStringExtra("cliente_nombre")
+        val documento = intent.getStringExtra("cliente_documento")
+        val direccion = intent.getStringExtra("cliente_direccion")
+        val telefono = intent.getStringExtra("cliente_telefono")
+        val tipoDocumento = intent.getStringExtra("cliente_tipodoc")
+        val ciudad = intent.getStringExtra("cliente_ciudad")
+        val zona = intent.getStringExtra("cliente_ciudad")
+        val correo = intent.getStringExtra("correo")
+
+        findViewById<TextView>(R.id.textoNombre).text = "$nombre"
+        findViewById<TextView>(R.id.textoTipoDoc).text = "$tipoDocumento"
+        findViewById<TextView>(R.id.textoNumDoc).text = "$documento"
+        findViewById<TextView>(R.id.textoDireccion).text = "$direccion"
+        findViewById<TextView>(R.id.textoTelefono).text = "$telefono"
+        findViewById<TextView>(R.id.textoCiudad).text = "$ciudad"
+        findViewById<TextView>(R.id.textoZona).text = "$zona"
+        findViewById<TextView>(R.id.textoCorreo).text = "$correo"
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
